182006c1ad718ecda7ad7eb92d5d48603d4049f0
Mirza Shahyaar Ahmed Baig <shahyaarmirza@gmail.com>
2024-07-20 04:51:09 -0700

    Created model_presistence.py

--- presistence/model_presistence.py
+++ presistence/model_presistence.py
@@ -2,13 +2,19 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 
-music_data=pd.read_csv('music.csv')
-x=music_data.drop(columns=['genre'])
-y=music_data['genre']
+# Load the dataset into a DataFrame
+music_data = pd.read_csv('music.csv')
 
-model=DecisionTreeClassifier()
-model.fit(x,y)
+# Separate features and target variable
+x = music_data.drop(columns=['genre'])  # Features
+y = music_data['genre']  # Target
 
-joblib.dump(model,'music-recommender.joblib')
+# Create and train the Decision Tree model
+model = DecisionTreeClassifier()
+model.fit(x, y)
+
+# Save the trained model to a file using joblib
+joblib.dump(model, 'music-recommender.joblib')
 
 # predictions=model.predict([[21,1],[22,0]])
+# print(predictions)
